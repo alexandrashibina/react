78456502f7e97a35591abef68fa17a2d08b0fc33
Alexandra Shibina <alexandrashibina@gmail.com>
2021-02-04 21:52:36 +0300

    partly fixed app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import App from './App';
 import { Provider } from 'react-redux';
 import {Router} from 'react-router-dom';
 import { createMemoryHistory} from 'history';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
-jest.mock("./map", () => ({ Map: () => <Map/> }));
+jest.mock("mapbox-gl", () => ({
+  Map: jest.fn(() => ({ remove: () => {} })),
+}));
 
 describe('App', () => {
 
@@ -29,26 +30,29 @@ describe('App', () => {
     expect(AppTree).toMatchSnapshot();
   });
 
-  test("opens correct pages when clicked on buttons", () => {
+  it("opens correct pages when clicked on buttons", () => {
     const mockStore = {
       getState: () => ({auth: {isLoggedIn: true}}),
       subscribe: () => {},
       dispatch: () => {},
     };
     const history = createMemoryHistory();
-    history.push('/profile')
-    const {getByTestId, getByText} = render(
+    history.push("/map")
+    // history.push("/profile")
+    const {getByTestId} = render(
       <Router history={history}>
         <Provider store={mockStore}>
           <App />
         </Provider>
       </Router> 
     );
-    // expect(getByTestId('header')).toBeInTheDocument()
-    // fireEvent.click(getByText('Профиль'))
-    expect(getByTestId('profile')).toBeInTheDocument()
+
+    expect(getByTestId("header")).toBeInTheDocument()
     
-    // userEvent.click(screen.getByText('Профиль'));
-    // expect(screen.getByTestId('profile')).toBeInTheDocument()
+    // fireEvent.click(getByText("Профиль"))
+    // expect(getByTestId("profile")).toBeInTheDocument()
+    
+    // fireEvent.click(getByText("Карта"));
+    expect(getByTestId("map")).toBeInTheDocument()
   });
 });
